ffa3d7e2374104a528ec0d3ef444cba0bc8be54c
Alexander Hrachovina <alexander.hrachovina@stud.sti.se>
2024-11-18 11:29:01 +0100

    05_filtering

---
+++ Live_demos/05_filtering/sql/where_clause.sql
@@ -0,0 +1,42 @@
+DESC;
+
+SELECT
+	*
+FROM
+	main.data_jobs dj;
+
+SELECT
+	work_year
+FROM
+	main.data_jobs dj;
+
+SELECT
+	COUNT(*)
+FROM
+	main.data_jobs;
+
+SELECT
+	*
+FROM
+	main.data_jobs dj
+WHERE
+	salary_in_usd < 50000;
+
+SELECT
+	COUNT(*) AS number_rows_lower_than_50k
+FROM
+	main.data_jobs dj
+WHERE
+	salary_in_usd < 50000;
+
+SELECT
+	DISTINCT experience_level
+FROM
+	main.data_jobs dj;
+	
+SELECT * FROM main.data_jobs WHERE experience_level = 'EN';
+
+SELECT MEDIAN(salary_in_usd) FROM main.data_jobs WHERE experience_level = 'EN';
+
+SELECT MEDIAN(salary_in_usd) FROM main.data_jobs WHERE experience_level = 'MI';
+
